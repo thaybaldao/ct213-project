482f5f33f1992cea2891daa969e58832284feb52
Gus <lincon.glds@gmail.com>
2021-07-21 10:17:08 -0300

    Adicionando funções"

--- model.py
+++ model.py
@@ -3,10 +3,18 @@ import torch as th
 
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3 import PPO
+import JSON
+from typing import Callable
 
 class RLAlgorithm:
     def __init__(self):
         raise NotImplementedError('Please implement this method')
+
+    def linear_schedule(initial_value: float) -> Callable[[float], float]:
+        def func(progress_remaining: float) -> float:
+            return progress_remaining * initial_value
+
+        return func
 
     def create_model(self):
         raise NotImplementedError('Please implement this method')
@@ -25,6 +33,10 @@ class RLAlgorithm:
         print("mean_reward:", mean_reward)
         print("std_reward:", std_reward)
 
+    def saveJSON(self, file_name, dictionary):
+        with open(file_name + ".json", "w") as outfile:
+            json.dump(dictionary, outfile, indent=4)
+
     def load_model(self):
         raise NotImplementedError('Please implement this method')
 
@@ -36,22 +48,19 @@ class RLAlgorithm:
             obs, rewards, dones, info = self.env.step(action)
             self.env.render()
 
-
 class PPO(RLAlgorithm):
     def __init__(self):
         # Create environment
         self.env = gym.make('LunarLander-v2')
 
     def load_hyperparameters(self):
+        pass
 
 
     def create_model(self):
         # Create the agent
         self.model_name = "ppo_lunar"
-        self.model = PPO("MlpPolicy", self.env, policy_kwargs=policy_kwargs, verbose=1)
+        self.model = PPO("MlpPolicy", self.env, policy_kwargs=policy_kwargs, verbose=1, learning_rate=linear_schedule(0.001))
 
     def load_model(self):
         self.model = PPO.load(self.model_name, env=self.env)
-
-class Hyperparameters:
-
